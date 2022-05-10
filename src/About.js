6d71a2b42b36110de955b528e57e8d42cc145eb6
plymth <plymth@protonmail.com>
2022-05-10 21:13:55 +0200

    feat: updated about copy

--- src/About.js
+++ src/About.js
@@ -7,7 +7,16 @@ const About = () => {
         Pouch is a simple wallet, which allows users to view, send and receive
         digital assets on the PTE API Service.
       </p>
-      <p>More features coming soon.</p>
+      <p>
+        In order to use Pouch you will need to download the{' '}
+        <a
+          href="https://docs.radixdlt.com/main/scrypto/public-test-environment/pte-getting-started.html"
+          target="_blank"
+        >
+          PTE Browser Extension
+        </a>
+        .
+      </p>
     </div>
   );
 };
